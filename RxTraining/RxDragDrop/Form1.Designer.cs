34273f1639124ca9d50bb9d043aef4329ac848cc
Aaron Ford <aaronjamesford@gmail.com>
2014-04-09 10:10:42 +0100

    Add drag and drop project & task

---
+++ RxTraining/RxDragDrop/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace RxDragDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bluePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // bluePanel
+            // 
+            this.bluePanel.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.bluePanel.Location = new System.Drawing.Point(38, 329);
+            this.bluePanel.Name = "bluePanel";
+            this.bluePanel.Size = new System.Drawing.Size(200, 147);
+            this.bluePanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 522);
+            this.Controls.Add(this.bluePanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel bluePanel;
+    }
+}
+
---
+++ RxTraining/RxDragDrop/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reactive.Linq;
+
+namespace RxDragDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Drag(this.bluePanel).Subscribe(p => this.bluePanel.Location = p);
+        }
+
+        private static IObservable<Point> Drag(Control ctrl)
+        {
+            // TODO #10 Modify this code to implement a drag & drop using Reactive Extensions
+            //          You should have learned all the tools so far to do this
+            //          Below are some observables created from events. you will need these.
+            //          The TakeUntil extension method will only take elements from the observable
+            //          until either a specified time or until an element on another observable is published.
+
+            var mousedown = Observable.FromEventPattern<MouseEventArgs>(ctrl, "MouseDown")
+                .Select(args => new Size(args.EventArgs.Location));
+            var mousemove = Observable.FromEventPattern<MouseEventArgs>(ctrl, "MouseMove")
+                .Select(args => new Size(args.EventArgs.Location));
+            var mouseup = Observable.FromEventPattern<MouseEventArgs>(ctrl, "MouseUp");
+
+            return Observable.Empty<Point>();
+        }
+    }
+}
