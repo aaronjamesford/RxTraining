e68a6cd1fc6c3cddc05e7ab768748543ba9faaba
Aaron Ford <aaronjamesford@gmail.com>
2014-04-10 12:56:32 +0100

    Add autocomplete task

---
+++ RxTraining/RxAutocomplete/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace RxAutocomplete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.resultsBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(13, 13);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(175, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // resultsBox
+            // 
+            this.resultsBox.FormattingEnabled = true;
+            this.resultsBox.Location = new System.Drawing.Point(13, 39);
+            this.resultsBox.Name = "resultsBox";
+            this.resultsBox.Size = new System.Drawing.Size(175, 212);
+            this.resultsBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 262);
+            this.Controls.Add(this.resultsBox);
+            this.Controls.Add(this.searchBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.ListBox resultsBox;
+    }
+}
+
---
+++ RxTraining/RxAutocomplete/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RxAutocomplete
+{
+    using System.Net;
+    using System.Reactive.Concurrency;
+    using System.Reactive.Disposables;
+    using System.Reactive.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Threading;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    using System.Xml.XPath;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.resultsBox.Items.Clear();
+
+            // TODO #11 Complete the code here to implement an autocomplete feature.
+            //      - Only search if the text is longer than 2 characters
+            //      - Make sure you do not search if the text has not changed (pressing arrow keys etc)
+            //      - Throttle the keyup events so that you only search a maximum of once every 500ms
+            //      - Only subscribe to the SearchWikipedia observable until another keyup event occurs
+            //      - Convert the result of SearchWikipedia into a list of strings
+            //      - Be sure to observe on the dispatcher thread (Dispatcher.CurrentDispatcher)
+            var keyup = Observable.FromEventPattern<KeyEventArgs>(this.searchBox, "KeyUp");
+        }
+
+        private static IObservable<string> SearchWikipedia(string query)
+        {
+            const string url = "http://en.wikipedia.org/w/api.php?action=opensearch&format=xml&search=";
+            var webClient = new WebClient();
+            var nsManager = new XmlNamespaceManager(new NameTable());
+            nsManager.AddNamespace("os", "http://opensearch.org/searchsuggest2");
+            var cancellable = new CancellationDisposable();
+            var asyncCancel = Disposable.Create(webClient.CancelAsync);
+
+            // Observable.Create is similar to using a Subject
+            // Here it's used so the asynchronous request can be cancelled on unsubscribe.
+            return Observable.Create<string>(observer =>
+                {
+                    webClient.DownloadStringCompleted += (sender, args) =>
+                        {
+                            if(args.Error != null || args.Cancelled)
+                            {
+                                observer.OnCompleted();
+                                return;
+                            }
+
+                            var xmlDoc = XDocument.Parse(args.Result);
+                            var items = xmlDoc.XPathSelectElements("/os:SearchSuggestion/os:Section/os:Item/os:Text", nsManager);
+
+                            foreach(var item in items.TakeWhile(item => !cancellable.Token.IsCancellationRequested))
+                            {
+                                observer.OnNext(item.Value);
+                            }
+
+                            observer.OnCompleted();
+                        };
+
+                    webClient.DownloadStringAsync(new Uri(url + query));
+
+                    return new CompositeDisposable(cancellable, asyncCancel, webClient);
+                });
+        }
+    }
+}
